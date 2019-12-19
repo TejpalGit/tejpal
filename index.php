3d1c2b0013313e94cacb651fcf17a5a3e3b9e937
TejpalGit <56465773+TejpalGit@users.noreply.github.com>
2019-12-19 12:10:21 +0530

    furthe changes

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 echo "index.php new file";
 ?>
-<a href="test.html">Test.html</a>
+<a href="test.html">Test.html</a><br>
+<a href="new.php">New.php</a>
---
+++ new.php
@@ -0,0 +1,2 @@
+<p>This is new.php</p>
+<a href="index.php">Main page</a>
